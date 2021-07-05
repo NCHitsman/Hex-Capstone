c85181d1fa84e0fff20d5137c79e18d2053b321b
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-07-04 19:20:45 -0700

    added threejs hex map

--- frontend/src/components/CreateMap/Hex.js
+++ frontend/src/components/CreateMap/Hex.js
@@ -1,10 +1,39 @@
+import { useRef, useState } from "react"
 
+const Hex = ({pos, x, y}) => {
 
-const Hex = ({q, r}) => {
-
+    const mesh = useRef()
+    const [hovered, setHover] = useState(false);
+    const [color, setColor] = useState('#171717')
+    const [visable, setVisable] = useState(false)
+    const [clicked, setClicked] = useState(false)
 
     return (
         <>
+            <mesh
+            ref={mesh}
+            position={pos}
+            onPointerOver={(e) => {
+                if (!clicked) {
+                    setHover(true)
+                    setVisable(true)
+                }
+            }}
+            onPointerOut={(e) => {
+                setHover(false)
+                if (!clicked) {
+                    setVisable(false)
+                }
+            }}
+            onClick={() => {
+                setColor('white')
+                setClicked(true)
+                setVisable(true)
+            }}
+            >
+                <cylinderBufferGeometry attach='geometry' args={[0.92, 0.92, 0.001, 6]}/>
+                <meshBasicMaterial color={hovered ? 'red' : color} visible={visable}/>
+            </mesh>
         </>
     )
 }
--- frontend/src/components/CreateMap/index.js
+++ frontend/src/components/CreateMap/index.js
@@ -1,36 +1,47 @@
-import { HexGrid, Layout, Pattern } from 'react-hexgrid';
 import './CreateMap.css'
-import Hex from './Hex';
+import {Canvas} from '@react-three/fiber'
+import Hex from './Hex'
+import {range} from 'lodash'
 
 
 const CreateMap = () => {
 
-    const array = []
-    let counter = 0
+    const mapSizeArray = range(25).map(() => range(25))
 
-    for (let i = 1; i <= 25; i++) {
-        let x = []
-        for (let j = 1; j <= 25; j++) {
-            x.push([])
-        }
-        array.push(x)
-    }
+    let x = -23.62
+    let y = -19.63
+    let eachOther = true
 
     return (
         <>
+            <Canvas
+            className="WorldCanvas"
+            camera={{ fov: 75, near: 0.1, far: 1000, position: [(90 * Math.PI) / 180, 30, 0] }}
+            >
+                {/* <axesHelper /> */}
+                {/* <gridHelper args={[50, 50]} position={[0,0,0]} /> */}
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                {mapSizeArray.map((b,i) => {
+                    y += 1.51
 
-            {/* {array.map((a, i) => {
-                counter += .5
-                return (
-                    a.map((b, j) => {
+                    if (eachOther) {
+                        x = -23.62
+                        eachOther = false
+                    } else {
+                        x = -24.51
+                        eachOther = true
+                    }
+
+                    return (
+                    b.map((l,p) => {
+                        x += 1.74
                         return (
-                            <>
-                            <Hex key={[i,j]} q={i-12} r={(j - 5) - Math.ceil(counter)} />
-                            </>
+                            <Hex key={i+p+x+y} pos={[x, 0.3, y]} x={i} y={p}/>
                         )
-                    })
-                )
-            })} */}
+                    }))
+                })}
+            </Canvas>
         </>
     )
 }
