756068475998aae5993d216cf8c672e9f5d5a4b5
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-07-09 13:38:49 -0700

    remove style tag

--- frontend/src/components/Navigation/ProfileButton.js
+++ frontend/src/components/Navigation/ProfileButton.js
@@ -21,9 +21,6 @@ function ProfileButton({ user }) {
   return (
     <div className='LogOutSignUpButtonCont'>
       <button className='LogOutSignUpButton' onClick={logout}>LOG OUT
-        <style>
-          @import url('https://fonts.googleapis.com/css2?family=Open+Sans&display=swap');
-        </style>
       </button>
     </div>
   );
