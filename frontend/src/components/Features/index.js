c5883d5ec98d1541e47334b684cf9965eb271170
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-08-08 13:11:10 -0700

    testing gifs

--- frontend/src/components/Features/index.js
+++ frontend/src/components/Features/index.js
@@ -1,11 +1,15 @@
+import gif from '../../images/example.gif'
+import './Features.css'
 
 const FeatureCard = () => {
-    return(
+    return (
         <div>
             <div>
                 <div>
                     <div></div>
-                    <div></div>
+                    <div className='FeatureCardGifCont'>
+                        <img className='FeatureCardGif' src={gif} alt='Example Usage of App' />
+                    </div>
                 </div>
                 <div>
 
