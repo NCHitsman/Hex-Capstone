cb61e998f19dd2524cca036606aeebd48401cd22
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-09-22 12:30:40 -0700

    working on layout of features page

--- frontend/src/components/Features/index.js
+++ frontend/src/components/Features/index.js
@@ -2,40 +2,49 @@ import gif from '../../images/example.gif'
 import './Features.css'
 import backgroundImage from '../../images/headshot.jpg'
 
-const FeatureCard = () => {
-    return (
-        <div>
-            <div>
-                <div>
-                    <div></div>
-                    <div className='FeatureCardGifCont'>
-                        <img className='FeatureCardGif' src={gif} alt='Example Usage of App' />
-                    </div>
-                </div>
-                <div>
+// const FeatureCard = () => {
+//     return (
 
-                </div>
-            </div>
-            <div>
-
-            </div>
-            <img
-            className='BackgroundImage'
-            src={backgroundImage}
-            alt='' />
-        </div>
-    )
-}
+//     )
+// }
 
 const Features = () => {
     return (
         <>
-            <div>
-                <FeatureCard side='left' />
-                <FeatureCard side='right' />
-                <FeatureCard side='left' />
-                <FeatureCard side='right' />
+            <div className='FeaturesParentCont'>
+                <div className='FeaturesSubCont'>
+                    <img className='FeaturesGif Top' src={gif} />
+                    <div>
+                        <div>Title</div>
+                        <div>Esse duis reprehenderit ullamco esse eu commodo id ut pariatur non irure ea. Elit et consequat nostrud nisi amet elit fugiat id fugiat aliquip.Ea laborum elit Lorem dolore magna pariatur fugiat non non aliqua sunt veniam ad.Sint ex quis proident voluptate quis velit.</div>
+                    </div>
+                </div>
+                <div className='FeaturesSubCont Bottom'>
+                    <div>
+                        <div>Title</div>
+                        <div>Esse duis reprehenderit ullamco esse eu commodo id ut pariatur non irure ea. Elit et consequat nostrud nisi amet elit fugiat id fugiat aliquip.Ea laborum elit Lorem dolore magna pariatur fugiat non non aliqua sunt veniam ad.Sint ex quis proident voluptate quis velit.</div>
+                    </div>
+                    <img className='FeaturesGif' src={gif} />
+                </div>
+                <div className='FeaturesSubCont Top'>
+                    <div>
+                        <div>Title</div>
+                        <div>Esse duis reprehenderit ullamco esse eu commodo id ut pariatur non irure ea. Elit et consequat nostrud nisi amet elit fugiat id fugiat aliquip.Ea laborum elit Lorem dolore magna pariatur fugiat non non aliqua sunt veniam ad.Sint ex quis proident voluptate quis velit.</div>
+                    </div>
+                    <img className='FeaturesGif' src={gif} />
+                </div>
+                <div className='FeaturesSubCont Bottom'>
+                    <img className='FeaturesGif' src={gif} />
+                    <div>
+                        <div>Title</div>
+                        <div>Esse duis reprehenderit ullamco esse eu commodo id ut pariatur non irure ea. Elit et consequat nostrud nisi amet elit fugiat id fugiat aliquip.Ea laborum elit Lorem dolore magna pariatur fugiat non non aliqua sunt veniam ad.Sint ex quis proident voluptate quis velit.</div>
+                    </div>
+                </div>
             </div>
+            <img
+                className='BackgroundImage'
+                src={backgroundImage}
+                alt='' />
         </>
     )
 }
