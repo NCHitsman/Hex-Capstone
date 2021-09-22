54e56c6d0afcb4abec58f7a30836922e41e12cb7
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-09-22 13:31:14 -0700

    working on text for features page

--- frontend/src/components/Features/index.js
+++ frontend/src/components/Features/index.js
@@ -13,46 +13,46 @@ const Features = () => {
         <>
             <div className='FeaturesParentCont'>
                 <div className='FeaturesSubCont'>
-                    <div className='FeaturesGifCont'>
-                        <img className='FeaturesGif Top' src={gif} />
+                    <div className='FeaturesGifCont Top'>
+                        <img className='FeaturesGif' alt='Example Gif' src={gif} />
                     </div>
-                    <div>
-                        <div>
-                            <div>Title</div>
-                            <div>Esse duis reprehenderit ullamco esse eu commodo id ut pariatur non irure ea. Elit et consequat nostrud nisi amet elit fugiat id fugiat aliquip.Ea laborum elit Lorem dolore magna pariatur fugiat non non aliqua sunt veniam ad.Sint ex quis proident voluptate quis velit.</div>
-                            <div>
-                                <div>Title</div>
-                                <div>Esse duis reprehenderit ullamco esse eu commodo id ut pariatur non irure ea. Elit et consequat nostrud nisi amet elit fugiat id fugiat aliquip.Ea laborum elit Lorem dolore magna pariatur fugiat non non aliqua sunt veniam ad.Sint ex quis proident voluptate quis velit.</div>
-                            </div>
+                    <div className='FeatureParentTextCont'>
+                        <div className='FeaturesTextCont Left'>
+                            <div className='FeaturesTitle'>Create Games</div>
+                            <div className='FeaturesText'>Create a System to be the home for your Planetary Empires Campaign. Each System will hold all of your maps, players, and teams. </div>
+                        </div>
+                        <div className='FeaturesTextCont Right'>
+                            <div className='FeaturesTitle'>Make Your Own Maps</div>
+                            <div className='FeaturesText'>Using the map creator, you can draw out hexigonal maps to use within your Campaign.</div>
                         </div>
                     </div>
-                    <div className='FeaturesGifCont'>
-                        <img className='FeaturesGif' src={gif} />
+                    <div className='FeaturesGifCont TopRight Bot'>
+                        <img className='FeaturesGif' alt='Example Gif' src={gif} />
                     </div>
                 </div>
-                {/* <div className='FeaturesSubCont Bottom'>
-                </div> */}
-                <div className='FeaturesSubCont Top'>
-                    <div>
-                        <div>Title</div>
-                        <div>Esse duis reprehenderit ullamco esse eu commodo id ut pariatur non irure ea. Elit et consequat nostrud nisi amet elit fugiat id fugiat aliquip.Ea laborum elit Lorem dolore magna pariatur fugiat non non aliqua sunt veniam ad.Sint ex quis proident voluptate quis velit.</div>
+                <div className='FeaturesSubCont Bot'>
+                    <div className='FeaturesGifCont BottomLeft Top'>
+                        <img className='FeaturesGif' alt='Example Gif' src={gif} />
                     </div>
-                    <div className='FeaturesGifCont'>
-                        <img className='FeaturesGif' src={gif} />
+                    <div className='FeatureParentTextCont Bot'>
+                        <div className='FeaturesTextCont Left'>
+                            <div className='FeaturesTitle'>Invite Your Players</div>
+                            <div className='FeaturesText'></div>
+                        </div>
+                        <div className='FeaturesTextCont Right'>
+                            <div className='FeaturesTitle'>Play the Game</div>
+                            <div className='FeaturesText'></div>
+                        </div>
+                    </div>
+                    <div className='FeaturesGifCont Bot'>
+                        <img className='FeaturesGif' alt='Example Gif' src={gif} />
                     </div>
                 </div>
-                {/* <div className='FeaturesSubCont Bottom'>
-                    <img className='FeaturesGif' src={gif} />
-                    <div>
-                        <div>Title</div>
-                        <div>Esse duis reprehenderit ullamco esse eu commodo id ut pariatur non irure ea. Elit et consequat nostrud nisi amet elit fugiat id fugiat aliquip.Ea laborum elit Lorem dolore magna pariatur fugiat non non aliqua sunt veniam ad.Sint ex quis proident voluptate quis velit.</div>
-                    </div>
-                </div> */}
             </div>
             <img
                 className='BackgroundImage'
                 src={backgroundImage}
-                alt='' />
+                alt='Unnamed by Games Workshop; https://www.warhammer-community.com/2019/10/16/space-marines-preview-the-imperial-fistsgw-homepage-post-1/#gallery-1' />
         </>
     )
 }
