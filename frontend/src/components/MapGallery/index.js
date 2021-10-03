75e4f8d078dcd4f6c735bf689e511b9f9474d11b
Noah Carmichael-Hitsman <noah.hitsman@gmail.com>
2021-10-02 19:11:10 -0700

    adding gifs

--- frontend/src/components/MapGallery/index.js
+++ frontend/src/components/MapGallery/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import gif from '../../images/example.gif'
+import map1 from '../../images/map1.PNG'
+import map2 from '../../images/map2.PNG'
+import map3 from '../../images/map3.PNG'
 import './MapGallery.css'
 import backgroundImage from '../../images/factory.jpg'
 
 const handleDragStart = (e) => e.preventDefault();
 
 const items = [
-    <img className='GalleryImg' alt='MapExampleImg' src={gif} onDragStart={handleDragStart} />,
-    <img className='GalleryImg' alt='MapExampleImg' src={gif} onDragStart={handleDragStart} />,
-    <img className='GalleryImg' alt='MapExampleImg' src={gif} onDragStart={handleDragStart} />,
+    <img className='GalleryImg' alt='MapExampleImg' src={map1} onDragStart={handleDragStart} />,
+    <img className='GalleryImg' alt='MapExampleImg' src={map2} onDragStart={handleDragStart} />,
+    <img className='GalleryImg' alt='MapExampleImg' src={map3} onDragStart={handleDragStart} />,
 ];
 
 const MapGallery = () => {
